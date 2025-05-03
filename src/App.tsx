1de307400ee81bc2b586435fa6f49adad3c802ab
saurabh12571257 <davesaurabh59@gmail.com>
2025-05-03 19:57:39 +0530

    routing and layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 
-import './App.css'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from './components/layout';
+import { ThemeProvider } from './context/theme-provider';
+import WeatherDashboard from './pages/weather-dashboard';
+import CityPage from './pages/city-page';
 
 function App() {
   
 
   return (
    <BrowserRouter>
-        <Layout> Hello </Layout>
+   <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+    </ThemeProvider>
    </BrowserRouter>
   )
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({children}: PropsWithChildren) => {
         <main className = "min-h-screen container mx-auto px-4 py-8" >
         {children}
         </main>
-        <footer className='border-t backdrop-blur py-12'>
+        <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'>
             <div className='container mx-auto px-4 text-center text-gray-400'>
                <p>
                 Made my Saurabh Dave
