13da7d1970fd96ea4f7c9339a38137f3b6957e6e
saurabh12571257 <davesaurabh59@gmail.com>
2025-05-03 11:29:18 +0530

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import {BrowserRouter} from "react-router-dom";
+import Layout from './components/layout';
 
 function App() {
   
 
   return (
    <BrowserRouter>
-
+        <Layout> Hello </Layout>
    </BrowserRouter>
   )
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,7 +8,13 @@ const Layout = ({children}: PropsWithChildren) => {
         <main className = "min-h-screen container mx-auto px-4 py-8" >
         {children}
         </main>
-        footer
+        <footer className='border-t backdrop-blur py-12'>
+            <div className='container mx-auto px-4 text-center text-gray-400'>
+               <p>
+                Made my Saurabh Dave
+               </p>
+            </div>
+        </footer>
     </div>
   );
 };
