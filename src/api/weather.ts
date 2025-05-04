efeaa62b0a5293d6be5a64ec78ee47aa3030aeb0
saurabh12571257 <davesaurabh59@gmail.com>
2025-05-04 15:02:00 +0530

    weather.ts api

---
+++ src/api/weather.ts
@@ -0,0 +1,47 @@
+import { error } from "console";
+import { API_CONFIG } from "./config";
+import { Coordinates, WeatherData } from "./types";
+
+class WeatherAPI{
+    private createUrl(endpoint: string, params: Record<string, string | number>){
+        const searchParams = new URLSearchParams({
+            appid: API_CONFIG.API_KEY,
+            ...params,
+        });
+        return `${endpoint}?${searchParams.toString()}`;
+    }
+
+    private fetchData<T>(url: string): Promise<T> {
+        const response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error(`Weather API ERROR: ${response.statusText}`);
+        }
+
+        return response.json();
+    }
+
+    async getCurrentWeather({lat, lon}: Coordinates): Promise<WeatherData>{
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`,{
+            lat:lat.toString(),
+            lon:lon.toString(),
+            units:API_CONFIG.DEFAULT_PARAMS.units,
+        });
+
+        return this.fetchData<WeatherData>(url);
+    }
+
+    async getForecast({lat, lon}: Coordinates): Promise<ForecastData>{
+        const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`,{
+            lat:lat.toString(),
+            lon:lon.toString(),
+            units:API_CONFIG.DEFAULT_PARAMS.units,
+        });
+
+        return this.fetchData<ForecastData>(url);
+    }
+
+    async reverseGeocode(){
+
+    }
+}
