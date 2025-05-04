abe322e16f84213640ec577690755a3601702e0a
saurabh12571257 <davesaurabh59@gmail.com>
2025-05-04 13:15:44 +0530

    header theme

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,10 @@
 import React, { PropsWithChildren } from 'react'
-
+import Header from './header';
 
 const Layout = ({children}: PropsWithChildren) => {
   return(
     <div className = "bg-gradient-to-br from-background to-muted" >
-        header
+        <Header/>
         <main className = "min-h-screen container mx-auto px-4 py-8" >
         {children}
         </main>
